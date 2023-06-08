1b6818068f0dcb36125e1dcf8f739ca6bf750b7a
RayanGuslya <yashka@mail.ru>
2023-06-08 17:38:35 +0300

    add and update main file

--- src/calculatirBeta.java
+++ src/calculatirBeta.java
@@ -1,52 +1,112 @@
-import java.io.File;
-import java.util.Scanner;
-import java.io.FileNotFoundException;
-public class calculatirBeta {
-    public static void main(String[] args) throws FileNotFoundException {
-        String separator = File.separator;
-        String html = "D:" + separator + "java" + separator + "ReadingAFile" + separator + "input.txt";
-        File file = new File(html);
-        Scanner scanner = new Scanner(file);
 
-        String num[] = scanner.nextLine().split(" ");
-        scanner.close();
-        double a, b;
-        boolean flag = true;
-        try{
-            a = Double.parseDouble(num[0]);
-            b = Double.parseDouble(num[2]);
-        }catch (NumberFormatException ex){
-            System.out.println("Error! Not number");
-            flag = false;
-            return;
-        }
-        if ("+-*/".contains(num[1]) && flag){
-            switch (num[1]){
-                case "+":{
-                    System.out.println(a+b);
-                    break;
-                }
-                case "-": {
-                    System.out.println(a-b);
-                    break;
-                }
-                case "*": {
-                    System.out.println(a*b);
-                    break;
-                } case "/": {
-                    if (b != 0.0){
-                        System.out.println(a/b);
-                    }else System.out.println("Error! Division by zero");
-//                    if (b ==0.0){
-//                        throw new ArithmeticException("Деление на ноль недопутсимо");
-//                    }
-//                    System.out.println(a/b);
-                    break;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class calculatirBeta {
+    public calculatirBeta() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+        String[] num = number.split(" ");
+
+        try {
+            FileWriter fileWriter = new FileWriter("input.txt", false);
+            Throwable var5 = null;
+
+            try {
+                fileWriter.write(number);
+            } catch (Throwable var26) {
+                var5 = var26;
+                throw var26;
+            } finally {
+                if (fileWriter != null) {
+                    if (var5 != null) {
+                        try {
+                            fileWriter.close();
+                        } catch (Throwable var25) {
+                            var5.addSuppressed(var25);
+                        }
+                    } else {
+                        fileWriter.close();
                     }
+                }
 
             }
-        }else {
-            System.out.println("Operation Error!");
+        } catch (IOException var29) {
+            System.out.println("Что то пошло не так " + var29.getMessage());
         }
+
+        scanner.close();
+        double a = 0.0;
+        double b = 0.0;
+        boolean flag = true;
+
+        try {
+            FileWriter fileWriter = new FileWriter("output.txt", false);
+
+            try {
+                a = Double.parseDouble(num[0]);
+                b = Double.parseDouble(num[2]);
+            } catch (NumberFormatException var23) {
+                String error = "Error! Not number";
+                System.out.println(error);
+                fileWriter.write(error);
+                fileWriter.close();
+                return;
+            }
+        } catch (Exception var24) {
+            System.out.println("Fuck");
+        }
+
+        try {
+            FileWriter fileWriter = new FileWriter("output.txt", false);
+            if ("+-*/".contains(num[1]) && flag) {
+                double answer;
+                switch (num[1]) {
+                    case "+":
+                        answer = a + b;
+                        System.out.println(answer);
+                        fileWriter.write(Double.toString(answer));
+                        fileWriter.close();
+                        break;
+                    case "-":
+                        answer = a - b;
+                        System.out.println(answer);
+                        fileWriter.write(Double.toString(answer));
+                        fileWriter.close();
+                        break;
+                    case "*":
+                        answer = a * b;
+                        System.out.println(answer);
+                        fileWriter.write(Double.toString(answer));
+                        fileWriter.close();
+                        break;
+                    case "/":
+                        if (b != 0.0) {
+                            answer = a / b;
+                            System.out.println(answer);
+                            fileWriter.write(Double.toString(answer));
+                            fileWriter.close();
+                        } else {
+                            String error = "Error! Division by zero";
+                            System.out.println(error);
+                            fileWriter.write(error);
+                            fileWriter.close();
+                        }
+                }
+            } else {
+                String error = "Operation Error!";
+                System.out.println(error);
+                fileWriter.write(error);
+                fileWriter.close();
+            }
+        } catch (IOException var27) {
+            System.out.println("Возникла ошибка" + var27.getMessage());
+        }
+
     }
 }
