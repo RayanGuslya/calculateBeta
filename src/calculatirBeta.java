31f9e05b00a9032668f061d6caf31240b2f76ee7
RayanGuslya <sesmeneev@mail.ru>
2023-06-09 19:33:27 +0300

    add update file

--- src/calculatirBeta.java
+++ src/calculatirBeta.java
@@ -14,11 +14,11 @@ public class calculatirBeta {
         String[] num = number.split(" ");
 
         try {
-            FileWriter fileWriter = new FileWriter("input.txt", false);
+            FileWriter fileWriter = new FileWriter("input.txt", true);
             Throwable var5 = null;
 
             try {
-                fileWriter.write(number);
+                fileWriter.write(number + "\n");
             } catch (Throwable var26) {
                 var5 = var26;
                 throw var26;
@@ -46,62 +46,71 @@ public class calculatirBeta {
         boolean flag = true;
 
         try {
-            FileWriter fileWriter = new FileWriter("output.txt", false);
+            FileWriter fileWriter = new FileWriter("output.txt", true);
 
             try {
                 a = Double.parseDouble(num[0]);
                 b = Double.parseDouble(num[2]);
             } catch (NumberFormatException var23) {
                 String error = "Error! Not number";
+                String entry = (int) a + " "+ num[1]+ " " + (int) b + " = " +error + "\n";
                 System.out.println(error);
-                fileWriter.write(error);
+                fileWriter.write(entry);
                 fileWriter.close();
                 return;
             }
         } catch (Exception var24) {
-            System.out.println("Fuck");
+            System.out.println("Error");
         }
 
         try {
-            FileWriter fileWriter = new FileWriter("output.txt", false);
+            FileWriter fileWriter = new FileWriter("output.txt", true);
+            String entry;
             if ("+-*/".contains(num[1]) && flag) {
                 double answer;
+
                 switch (num[1]) {
                     case "+":
                         answer = a + b;
                         System.out.println(answer);
-                        fileWriter.write(Double.toString(answer));
+                        entry = ((int) a + " + " + (int) b + " = " +answer + "\n");
+                        fileWriter.write(entry);
                         fileWriter.close();
-                        break;
+                        return;
                     case "-":
                         answer = a - b;
                         System.out.println(answer);
-                        fileWriter.write(Double.toString(answer));
+                        entry = ((int)a + " - " +(int) b + " = " + answer + "\n");
+                        fileWriter.write(entry);
                         fileWriter.close();
                         break;
                     case "*":
                         answer = a * b;
                         System.out.println(answer);
-                        fileWriter.write(Double.toString(answer));
+                        entry = (int) a + " * " + (int) b + " = " +answer + "\n";
+                        fileWriter.write(entry);
                         fileWriter.close();
                         break;
                     case "/":
                         if (b != 0.0) {
                             answer = a / b;
                             System.out.println(answer);
-                            fileWriter.write(Double.toString(answer));
+                            entry = (int) a + " / " + (int) b + " = " +answer + "\n";
+                            fileWriter.write(entry);
                             fileWriter.close();
                         } else {
                             String error = "Error! Division by zero";
                             System.out.println(error);
-                            fileWriter.write(error);
+                            entry = (int) a + " / " + (int) b + " = " +error + "\n";
+                            fileWriter.write(entry);
                             fileWriter.close();
                         }
                 }
             } else {
                 String error = "Operation Error!";
                 System.out.println(error);
-                fileWriter.write(error);
+                entry = (int) a + " / " + (int) b + " = " +error + "\n";
+                fileWriter.write(entry);
                 fileWriter.close();
             }
         } catch (IOException var27) {
